2d20f34da26ffc9db1529725f674eda99b120bd5
esanchezlMBT <esanchezl@mainbit.com.mx>
2020-07-01 19:18:28 -0500

    Release 0.2
    Fix DialogBot

--- bots/bot.js
+++ bots/bot.js
@@ -3,29 +3,49 @@
 
 const { ActivityHandler, MessageFactory } = require('botbuilder');
 
-class EchoBot extends ActivityHandler {
-    constructor() {
+class DialogBot extends ActivityHandler {
+    constructor(conversationState, userState, dialog) {
         super();
+        this.conversationState = conversationState;
+        this.userState = userState;
+        this.dialog = dialog;
+        this.dialogState = this.conversationState.createProperty('DialogState');
+
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            const replyText = `Echo: ${ context.activity.text }`;
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
+            console.log('Running dialog with Message Activity.');
 
-        this.onMembersAdded(async (context, next) => {
-            const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome from GITHUB!';
-            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-                if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
-                }
-            }
+            // Run the Dialog with the new message Activity.
+            await this.dialog.run(context, this.dialogState);
+
+            // Save any state changes. The load happened during the execution of the Dialog.
+            await this.conversationState.saveChanges(context, false);
+            await this.userState.saveChanges(context, false);
+
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
+        
+        this.onDialog(async (context, next) => {
+            // Save any state changes. The load happened during the execution of the Dialog.
+            await this.conversationState.saveChanges(context, false);
+            await this.userState.saveChanges(context, false);
+
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+        // this.onMembersAdded(async (context, next) => {
+        //     const membersAdded = context.activity.membersAdded;
+        //     const welcomeText = 'Hello and welcome from GITHUB!';
+        //     for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+        //         if (membersAdded[cnt].id !== context.activity.recipient.id) {
+        //             await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+        //         }
+        //     }
+        //     // By calling next() you ensure that the next BotHandler is run.
+        //     await next();
+        // });
     }
 }
 
-module.exports.EchoBot = EchoBot;
+module.exports.DialogBot = DialogBot;
